dcb157c518294b77b816be8ebc754351c15f8f66
aayushsahu <ayush297@gmail.com>
2020-07-21 01:55:05 +0530

    Initial Commit

---
+++ Spring-Author-Book-Publisher/src/main/java/com/solinvictus/SpringAuthorBookPublisher/SpringAuthorBookPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.solinvictus.SpringAuthorBookPublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthorBookPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthorBookPublisherApplication.class, args);
+	}
+
+}
---
+++ Spring-Author-Book-Publisher/src/test/java/com/solinvictus/SpringAuthorBookPublisher/SpringAuthorBookPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solinvictus.SpringAuthorBookPublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthorBookPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
